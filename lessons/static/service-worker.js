db8ab6da94ffe6507a002c1d102b4d4c4778b750
Mhmhm1 <118804540+Mhmhm1@users.noreply.github.com>
2025-09-21 08:03:30 +0300

    Update service-worker.js

--- lessons/static/service-worker.js
+++ lessons/static/service-worker.js
@@ -1,1 +1,11 @@
+self.addEventListener('install', function(e) {
+    console.log('Service Worker: Installed');
+});
 
+self.addEventListener('activate', function(e) {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('Service Worker: Fetching', e.request.url);
+});
